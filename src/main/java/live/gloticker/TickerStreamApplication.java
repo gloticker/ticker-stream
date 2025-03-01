8611c71863639b2204c5d59bb1d4f40ac329f17a
takealook97 <takealook97@naver.com>
2025-03-02 03:26:38 +0900

    chore: build init

---
+++ src/main/java/live/gloticker/TickerStreamApplication.java
@@ -0,0 +1,13 @@
+package live.gloticker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickerStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/live/gloticker/TickerStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package live.gloticker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickerStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
