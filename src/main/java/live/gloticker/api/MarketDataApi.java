992c44f438e48d4d4a05ae9dc507dbc07bb31baa
takealook97 <takealook97@naver.com>
2025-03-06 11:01:39 +0900

    feat: add snapshot & chart get-method

--- src/main/java/live/gloticker/api/MarketDataApi.java
+++ src/main/java/live/gloticker/api/MarketDataApi.java
@@ -1,30 +1,41 @@
 package live.gloticker.api;
 
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import live.gloticker.service.MarketDataService;
+import jakarta.validation.constraints.Pattern;
+import live.gloticker.service.MarketService;
+import live.gloticker.service.StreamService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1")
+@RequestMapping("/v1/market")
 public class MarketDataApi {
-	private final MarketDataService marketDataService;
+	private final StreamService streamService;
+	private final MarketService marketService;
 
-	@GetMapping(value = "/market", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public SseEmitter streamMarketData() {
-		return marketDataService.subscribe();
+		return streamService.subscribe();
+	}
+
+	@GetMapping("/{type}")
+	public ResponseEntity<List<Object>> getData(@PathVariable("type") @Pattern(regexp = "snapshot|chart") String type) {
+		return ResponseEntity.ok(marketService.getAllData(type));
 	}
 
 	@ExceptionHandler(AsyncRequestTimeoutException.class)
-	public ResponseEntity<Void> handleAsyncRequestTimeoutException(AsyncRequestTimeoutException ex) {
+	public ResponseEntity<Void> handleAsyncRequestTimeoutException() {
 		return ResponseEntity.noContent().build();
 	}
 }
